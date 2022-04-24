73e425084b6e30b7aa9654a009486f5494f84293
nallapati <nallapati@sunil>
2022-04-24 15:30:51 +0530

    collection update 2.0

---
+++ src/collection/set.java
@@ -0,0 +1,17 @@
+package collection;
+
+import java.util.HashSet;
+
+public class set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      HashSet<String>hs=new HashSet<String>();
+      hs.add("sunil");
+      hs.add("venkat");
+      hs.add("sunil");
+      hs.add("aditya");
+      System.out.println(hs);
+	}
+
+}

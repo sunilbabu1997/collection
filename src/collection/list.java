46cd34c75e6aac5dd45787c1f5445ae5489967d5
nallapati <nallapati@sunil>
2022-04-24 15:23:09 +0530

    collection programme

---
+++ src/collection/list.java
@@ -0,0 +1,18 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class list {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 ArrayList<String>a=new ArrayList<String>();
+		 a.add("sunil");
+		 a.add("venkat");
+		 a.add("aditya");
+		 System.out.println(a);
+		 
+
+	}
+
+}
